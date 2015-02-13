e7fedfd0e30be76caba0b6ace8d8fdc548efa908
juno <juno@krasava.fi>
2015-02-13 07:11:50 +0200

    dev

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,16 +2,37 @@
 
 angular.module('angularImagehoster')
   .controller('MainCtrl', function ($scope,FileUploader,$resource) {
-	  $scope.uploader = new FileUploader({url:'http://localhost:8000/upload'});
-	  
-	  
+	  	  	  
+	  $scope.sex='female';
+	  $scope.age=25;
+	  $scope.topic='sex';
+	  var uploader =  $scope.uploader = new FileUploader({url:'http://localhost:8000/upload',removeAfterUpload:true }); 
+	  	  
 		var Character = $resource("http://localhost\\:8000/api/:id", {
 			id : '@id'
 		}, {});
 		
 		$scope.characters = Character.query();
-	  
-	  
-	  
-	  
+		
+	  	  
+	  uploader.onCompleteAll = function() {
+         
+          $scope.characters = Character.query();          
+          
+      };
+      
+      uploader.onBeforeUploadItem = function(item) {
+    	  
+    	  var formData = [{
+    	        age: $scope.age,
+    	    },{
+    	    	sex: $scope.sex,
+    	    },{
+    	    	topic :  $scope.topic,
+    	    }];
+    	    Array.prototype.push.apply(item.formData, formData);
+    	     	  
+
+    	};
+      	  	  
   });
